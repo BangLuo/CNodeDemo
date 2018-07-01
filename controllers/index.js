6c290fd36daa6657e8c33a28d8e41cbb98ddd020
LuoDan <1178886976@qq.com>
2018-07-01 20:27:14 +0800

    主页日期 发起人正常显示

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const topicModel = require('../models/topic');
+const moment = require('moment');
 exports.showIndex =(req,res)=>{
     
     topicModel.getAll((err,topics)=>{
         res.render('./index.html',{
             topics,
             user:req.session.user,
+            moment
             
         })
     })
